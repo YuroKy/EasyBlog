71b2ed8991e24acac82b3a09c3386cbf216ae877
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2020-12-15 01:04:45 +0200

    Add Posts Filter

--- clientapp/src/pages/posts/index.js
+++ clientapp/src/pages/posts/index.js
@@ -3,20 +3,47 @@ import Moment from "moment";
 export default {
   data() {
     return {
+      term: null,
+      tags: [],
+      filterTags: [],
       posts: []
     };
   },
   async mounted() {
-    var { data: posts } = await this.axios.get('posts');
-
-    if (!posts.length) {
-      this.$router.push({ name: "editor" });
-    }
-
-    posts.forEach(p => p.createdTime = Moment(p.createdTime).format("DD/MM/YYYY HH:mm"));
-
-    this.posts = posts;
+    await this.loadPosts();
+    await this.loadTags();
   },
   methods: {
+    async loadPosts() {
+      var { data: posts } = await this.axios.get('posts');
+      posts.forEach(p => p.createdTime = Moment(p.createdTime).format("DD/MM/YYYY HH:mm"));
+      this.posts = posts;
+    },
+    async loadTags() {
+      var { data: tags } = await this.axios.get('tags');
+      this.tags = tags;
+    },
+    async applyFilter() {
+      await this.loadPosts();
+
+      if (this.term) {
+        this.posts = this.posts.filter(p => p.content.toLowerCase().includes(this.term.toLowerCase()) || p.title.toLowerCase().includes(this.term.toLowerCase()));
+      }
+
+      if (this.filterTags.length) {
+        const filteredIds = this.filterTags.map(ft => ft.id);
+        this.posts = this.posts.filter(p => p.tags.map(t => t.id).some(id => filteredIds.includes(id)));
+      }
+
+      if (!this.term && !this.filterTags.length) {
+        await this.resetFilter();
+      }
+
+    },
+    async resetFilter() {
+      this.term = null;
+      this.filterTags = [];
+      await this.loadPosts();
+    }
   },
 };
